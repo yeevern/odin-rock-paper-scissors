5cb56e0bb333f05d042df314880b31a4e356da56
yeevern <yeevernchang@gmail.com>
2023-01-01 21:52:43 +0800

    done implementing game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let rounds = 1;
 const maxScore = 5;
 
 const options = ["rock", "paper", "scissors"]
@@ -12,68 +13,65 @@ function playRound(playerSelection, computerSelection) {
     console.log(playerSelection, computerSelection)
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    rounds++;
     if (playerSelection === "rock") {
         if (playerSelection < computerSelection) {
             playerScore++;
-            // return `You win! ${playerSelection} beats ${computerSelection}`
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            return `You win! ${playerSelection} beats ${computerSelection}`
         }
         else if (playerSelection > computerSelection) {
             computerScore++;
-            // return `You lose! ${playerSelection} beats ${computerSelection}`
-            console.log(`You lose! ${playerSelection} beats ${computerSelection}`);
+            return `You lose! ${playerSelection} beats ${computerSelection}`
         }
         else {
-            console.log(`Draw`);
+            return `Draw! Both pick ${playerSelection}, ${computerSelection}`;
         }
     }
     else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             playerScore++;
-            // return `You win! ${playerSelection} beats ${computerSelection}`
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            return `You win! ${playerSelection} beats ${computerSelection}`
         }
         else if (computerSelection === "scissors") {
             computerScore++
-            // return `You lose! ${playerSelection} beats ${computerSelection}`
-            console.log(`You lose! ${playerSelection} beats ${computerSelection}`);
+            return `You lose! ${playerSelection} beats ${computerSelection}`
         }
         else {
-            console.log(`Draw`);
+            return `Draw! Both pick ${playerSelection}, ${computerSelection}`;
         }
     }
     else {
         if (computerSelection === "paper") {
             playerScore++;
-            // return `You win! ${playerSelection} beats ${computerSelection}`
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            return `You win! ${playerSelection} beats ${computerSelection}`
         }
-        else if (computerScore === "rock") {
+        else if (computerSelection === "rock") {
             computerScore++;
-            // return `You lose! ${playerSelection} beats ${computerSelection}`
-            console.log(`You lose! ${playerSelection} beats ${computerSelection}`);
+            return `You lose! ${playerSelection} beats ${computerSelection}`
         }
         else {
-            console.log(`Draw`);
+            return `Draw! Both pick ${playerSelection}, ${computerSelection}`;
         }
     }
 }
 
 function game(playerSelection) {
     const computerSelection = getComputerChoice();
+    roundsDisplay.innerHTML = `ROUND ${rounds}`;
     playerScoreDisplay.innerHTML = playerScore.toString();
     computerScoreDisplay.innerHTML = computerScore.toString();
     resultDisplay.innerHTML = playRound(playerSelection, computerSelection);
-    selectionDisplay.innerHTML = `<li>Computer selected: ${computerSelection}</li> <li>Player selected: ${playerSelection}</li>`;
+    selectionDisplay.innerHTML = `<li>Player selected: ${playerSelection}</li> <li>Computer selected: ${computerSelection}</li>`;
     if (Math.max(playerScore, computerScore) === maxScore) {
         let winner = playerScore === maxScore ? "Player" : "Computer";
-        alert(`${winner} is the winner!`);
-        document.querySelectorAll('button').forEach(elem => elem.onclick = null);
+        alert(`${winner} is the winner!`);  // display the winner
+        document.querySelectorAll('button').forEach(elem => elem.onclick = null);  // set it to null so that it wont keep calling game() after concluding a winner
     } 
 }
 
+const roundsDisplay = document.querySelector('.rounds');
 const playerScoreDisplay = document.querySelector('.player-score');
 const computerScoreDisplay = document.querySelector('.computer-score');
 const resultDisplay = document.querySelector('.result');
 const selectionDisplay = document.querySelector('.selection');
-document.querySelectorAll('button').forEach(elem => elem.onclick = () => {game(elem.textContent);});
+document.querySelectorAll('button').forEach(elem => elem.onclick = () => {game(elem.textContent);});  // set attribute according to player selected button
